cfa4a211bd42e4266533fd12bd0b9953c345edfc
JHadwin1 <jeremiahhadwin@yahoo.com>
2024-03-29 15:36:36 -0400

    basic view for difficulty select menu

---
+++ app/src/main/java/com/example/myapplication/difficulty_menu/DifficultyMenu.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.difficulty_menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class DifficultyMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_difficulty_menu);
+    }
+}
