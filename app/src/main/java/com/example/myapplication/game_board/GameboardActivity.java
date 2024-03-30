43b9227faba6452b92595fdac3f3ba199eaeaf82
jeremiah hadwin <jeremiahhadwin@yahoo.com>
2024-03-29 22:01:09 -0400

    basic nav from gameboard to main menu

--- app/src/main/java/com/example/myapplication/game_board/GameboardActivity.java
+++ app/src/main/java/com/example/myapplication/game_board/GameboardActivity.java
@@ -2,9 +2,14 @@ package com.example.myapplication.game_board;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.myapplication.R;
+import com.example.myapplication.difficulty_menu.DifficultyMenu;
+import com.example.myapplication.main_menu.MainActivity;
 
 public class GameboardActivity extends AppCompatActivity {
 
@@ -12,5 +17,14 @@ public class GameboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gameboard);
+
+        Button backButton = (Button)findViewById(R.id.boardBackButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GameboardActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
