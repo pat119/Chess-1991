e6a6b6eed5ed58eaa003fad672fd1ea49ab71148
pat119 <pli373775@gmail.com>
2024-04-02 17:36:07 -0400

    Updated function to be able to change image source. This will pave the way to program moving pieces.

--- app/src/main/java/com/example/myapplication/game_board/GameboardActivity.java
+++ app/src/main/java/com/example/myapplication/game_board/GameboardActivity.java
@@ -2,10 +2,14 @@ package com.example.myapplication.game_board;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.myapplication.R;
 import com.example.myapplication.difficulty_menu.DifficultyMenu;
@@ -37,9 +41,20 @@ public class GameboardActivity extends AppCompatActivity {
 //            case R.id.button_send:
                 // Do something
         }
-        int[] location = new int[2];
-        view.getLocationInWindow(location);
-        view.setVisibility(View.INVISIBLE);
-//        finish();
+//        int[] location = new int[2];
+//        view.getLocationInWindow(location);
+
+        // First get name of button
+        Resources res = getResources();
+        String fullName = getResources().getResourceName(view.getId());
+        String name = fullName.substring(fullName.lastIndexOf("/") + 1);
+        // Use button name to get name of corresponding image
+        String imageName = name.substring(0, 2).concat("Image");
+        int id = res.getIdentifier(imageName, "id", getApplicationContext().getPackageName());
+        ImageView image = findViewById(id);
+        image.setImageResource(R.drawable.player_star);
+
+//        view.setVisibility(View.INVISIBLE);
+
     }
 }
