59531225d0c09e79adb9a7d479b06048bc5a7a77
Raul <arevaloraul31@gmail.com>
2024-04-06 00:47:27 -0400

    Raul endscreen current implementation

--- app/src/main/java/com/example/myapplication/checkmate/FragmentCheckmate.java
+++ app/src/main/java/com/example/myapplication/checkmate/FragmentCheckmate.java
@@ -4,14 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.myapplication.R;
-import com.example.myapplication.databinding.FragmentCheckmateBinding;
 
 public class FragmentCheckmate extends Fragment {
 
@@ -23,8 +21,19 @@ public class FragmentCheckmate extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_checkmate, container, false);
+        View view = inflater.inflate(R.layout.fragment_checkmate, container, false);
+
+        Button postLeaderBoard = view.findViewById(R.id.PostLeader);
+        postLeaderBoard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Leaderboard coming soon!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
+
 
 
 }
