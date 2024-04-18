8c7d6664de192f7a0ea19e425caf8e1795423b97
pat119 <pli373775@gmail.com>
2024-04-18 17:01:52 -0400

    began work on integrating signup and firebase

--- app/src/main/java/com/example/myapplication/welcome/SignupActivity.java
+++ app/src/main/java/com/example/myapplication/welcome/SignupActivity.java
@@ -4,18 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.myapplication.R;
 import com.example.myapplication.databinding.ActivitySignupBinding;
 import com.example.myapplication.databinding.ActivityStartBinding;
 import com.example.myapplication.main_menu.MainActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignupActivity extends AppCompatActivity {
     private ActivitySignupBinding binding;
 
     private View decorView;
+    EditText usernameInput;
+    EditText passwordInput;
+    EditText confirmPasswordInput;
+    DatabaseReference dbref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +36,6 @@ public class SignupActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         decorView = getWindow().getDecorView();
-
         decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
             @Override
             public void onSystemUiVisibilityChange(int visibility) {
@@ -31,10 +43,46 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
 
+        usernameInput = findViewById(R.id.usernameInput);
+        passwordInput = findViewById(R.id.passwordInput);
+        confirmPasswordInput = findViewById(R.id.confirmPasswordInput);
+
+        dbref = FirebaseDatabase.getInstance().getReference();
+
         Button signupConfirmButton = (Button)findViewById(R.id.SignupConfirmButton);
         signupConfirmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Get new username/password
+                String username = usernameInput.getText().toString();
+                String password = passwordInput.getText().toString();
+                String confirmPassword = confirmPasswordInput.getText().toString();
+
+                // Check that user has entered a username and password
+                if (username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Empty input field", Toast.LENGTH_SHORT);
+                    toast.show();
+                    return;
+                }
+
+                // Check there is no duplicate username
+//                String testa = dbref.child("logins").child(username).toString();
+//                Boolean test = dbref.child(username).exists();
+//                String test2 = dbref.child("logins").child(username);
+//                String test22 = dbref.child("logins").child("NONEXISTENT");
+
+                // Check that password is confirmed correctly
+                if (!password.equals(confirmPassword)) {
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            "Passwords must match", Toast.LENGTH_SHORT);
+                    toast.show();
+                    return;
+                }
+
+                // Add username/password to the database
+                dbref.child("logins").child(username).setValue(password);
+
                 Intent intent = new Intent(SignupActivity.this, MainActivity.class);
                 startActivity(intent);
             }
